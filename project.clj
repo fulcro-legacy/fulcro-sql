9f47301a2894d1ee9c4441de4f70542daf24d088
Tony Kay <tony.kay@gmail.com>
2017-10-12 12:53:46 -0700

    version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fulcrologic/fulcro-sql "0.2.0-SNAPSHOT"
+(defproject fulcrologic/fulcro-sql "0.2.0"
   :description "A library for using SQL databases as components, writing integration tests, and running Datomic-style graph queries against them."
   :url "http://github.com/fulcrologic/fulcro-sql"
   :license {:name "MIT Public License"}
@@ -25,7 +25,7 @@
                  [org.slf4j/jcl-over-slf4j "1.7.25" :scope "provided"]
                  [com.fzakaria/slf4j-timbre "0.3.7" :scope "provided"]
 
-                 [fulcrologic/fulcro-spec "1.0.0-beta8" :scope "test" :exclusions [org.clojure/tools.reader]]
+                 [fulcrologic/fulcro-spec "1.0.0-beta9" :scope "test" :exclusions [org.clojure/tools.reader]]
                  [clj-time "0.14.0" :scope "test"]
                  [org.mariadb.jdbc/mariadb-java-client "2.1.0" :scope "test"]
                  [org.postgresql/postgresql "42.1.4.jre7" :scope "test"]
